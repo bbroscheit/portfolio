1195360cac6dec386bb6ef3b251973ed2ba331e7
bbroscheit <bernardo.broscheit@gmail.com>
2023-01-18 12:01:37 -0300

    prueba de vercel

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header/Header.jsx';
 import Profile from './components/Profile/Profile.jsx';
 import Services from './components/Services/Services.jsx';
 import Form from './components/Form/Form.jsx';
-import Footer from './components/Footer/Footer.jsx';
+// import Footer from './components/Footer/Footer.jsx';
 import Portfolio from './components/Portfolio/Portfolio';
 
 
@@ -15,7 +15,7 @@ function App() {
       <Services />
       <Portfolio />
       <Form />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
